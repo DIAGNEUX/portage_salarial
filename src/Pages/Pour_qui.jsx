8fbb118d731337368886f85b080602a2b8888000
DIAGNEUX <aliouned007@gmail.com>
2023-08-27 18:13:16 +0200

    deploy

--- src/Pages/Pour_qui.jsx
+++ src/Pages/Pour_qui.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import "./css/Qui.css"
 import it_support from  "../Assets/illustrator/lawyer-c.png"
+import CRM from "../Assets/illustrator/chef CRM.png" ;
+import Directeur from '../Assets/illustrator/Directeur de projet.png'
+import Data_Analyst from "../Assets/illustrator/Data-analyst.png" ;
+import Dev_back from "../Assets/illustrator/dev backend.png"
+import Dev_front from "../Assets/illustrator/dev Front.png"
+import Dev_full from "../Assets/illustrator/developpeur full.png"
+import devOps from "../Assets/illustrator/devOps.png"
+import Owner from "../Assets/illustrator/Product Owner.png"
+import project_manager from "../Assets/illustrator/Project Manager.png"
 
 const Pour_qui = () => {
+  const informatique = [
+    {nom: "consultant informatique " , image : it_support } ,
+    {nom: "Data Analyst " , image : Data_Analyst },
+    {nom: "DevOps " , image : devOps },
+    {nom: "Developpeur FullStack " , image : Dev_full },
+    {nom: "Developpeur BackEnd " , image : Dev_back },
+    {nom: "Developpeur FrontEnd " , image : Dev_front },
+    {nom: "Product Owner " , image : Owner },
+    {nom: "Directeur de projet " , image : Directeur},
+    {nom: "Chef de projet CRM " , image : CRM }
+    
+
+  ]
   return (
     <div className='wrap_who'>
       <div>
@@ -21,12 +43,19 @@ const Pour_qui = () => {
          des consultants et un meilleur support à leur développement.
          </p>
 
+         <div className='wrap_jobs'>
          <div className='jobs'>
-          <div className='it'>        
-          <img src={it_support} alt="" />
-          <p>IT consulting</p>
-          </div>
+          {informatique.map((metiers_info)=> (
+            <div className='it' key={metiers_info.nom}>        
+            <img src={metiers_info.image} alt="" />
+            <p> {metiers_info.nom}  </p>
+            </div>
+
+          ))}
+          
          </div>
+         </div>
+
       </div>
     </div>
   )
